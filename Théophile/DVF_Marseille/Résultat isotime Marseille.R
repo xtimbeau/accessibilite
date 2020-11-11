d904da3e81681fb78f71e14dcc5d434d283621bd
Theophile-Gervais <71379428+Theophile-Gervais@users.noreply.github.com>
2020-11-11 15:46:32 +0100

    Résultat isotime Marseille

--- Théophile/DVF_Marseille/Résultat isotime Marseille.R
+++ Théophile/DVF_Marseille/Résultat isotime Marseille.R
@@ -1,47 +1,47 @@
 source("access.r")
 
-aqui.mbr <- load_DVF("aqui.mbr")
+paca.mbr <- load_DVF("paca.mbr")
 iris15 <- load_DVF("iris15")
-ttr_r5_emp09_Bordeaux <- load_DVF("ttr_r5_emp09_Bordeaux")
+ttr_r5_emp09_Marseille <- load_DVF("ttr_r5_emp09_Marseille")
 
 # Construction bb33701
 
-uu33701 <- iris15 %>% filter(UU2010=="33701") %>% st_union 
+uu759 <- iris15 %>% filter(UU2010=="00759") %>% st_union 
 uu851 <- iris15 %>% filter(UU2010=="00851") %>% st_union
 c_uu851 <- uu851 %>% st_centroid
-c_uu33701 <- uu33701 %>% st_centroid
-bb33701 <- st_bbox((uu851[[1]]-c_uu851[[1]]+c_uu33701[[1]]), crs=3035)
+c_uu759 <- uu759 %>% st_centroid
+bb759 <- st_bbox((uu851[[1]]-c_uu851[[1]]+c_uu759[[1]]), crs=3035)
 
 # construction uu33701$bbox
 
-daqui <- load_DVF("daqui")
-caqui <- load_DVF("caqui")
+dpaca <- load_DVF("dpaca")
+cpaca <- load_DVF("cpaca")
 
-uu33701 <- NULL
+uu759 <- NULL
 iris15 <- load_DVF("iris15")
-uu33701$iris <- iris15 %>% filter(UU2010=="33701") %>% st_union 
-caqui <- load_DVF("caqui") %>% st_transform(3035)
+uu759$iris <- iris15 %>% filter(UU2010=="00759") %>% st_union 
+cpaca <- load_DVF("cpaca") %>% st_transform(3035)
 
-uu33701$depsf <- daqui
+uu759$depsf <- dpaca
 
-uu33701$border <- caqui %>%
+uu759$border <- cpaca %>%
   filter(insee %chin%
            (iris15 %>%
-              filter(UU2010=="33701") %>%
+              filter(UU2010=="00759") %>%
               pull(COM) %>%
               unique)) %>%
   st_union
 
-uu33701$bbox <- st_bbox(uu33701$border)
+uu759$bbox <- st_bbox(uu759$border)
 
 # construction riv
 
 riv <- st_read("{DVFdata}/fdCartes/elthydrosurface/EltHydroSurface_FXX.shp" %>% glue) %>%
   st_transform(3035)
-riv <- riv %>% filter(Type=="Cours d'eau") %>% st_filter(uu33701$border) %>% st_crop(uu33701$border)
+riv <- riv %>% filter(Type=="Cours d'eau") %>% st_filter(uu759$border) %>% st_crop(uu759$border)
 
 # Cartes isotime sur fond de carte
 
-isotime_Bordeaux <- tm_shape(aqui.mbr,bbox=bb33701)+tm_rgb()+
-                    tm_shape(ttr_r5_emp09_Bordeaux)+tm_raster(style="cont",palette=heatrg)+
-                    tm_shape(riv,bbox=uu33701$bbox)+tm_fill("dodgerblue",alpha=1)
+isotime_Marseille <- tm_shape(paca.mbr,bbox=bb759)+tm_rgb()+
+                     tm_shape(ttr_r5_emp09_Marseille)+tm_raster(style="cont",palette=heatrg)+
+                     tm_shape(riv,bbox=uu759$bbox)+tm_fill("dodgerblue",alpha=1)
