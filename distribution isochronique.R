3b4c57b3e47d2c3e3207dfb63b129d00b81cf1fa
xtimbeau <54633745+xtimbeau@users.noreply.github.com>
2020-11-18 19:53:24 +0100

    r2dt  corrigé

--- distribution isochronique.R
+++ distribution isochronique.R
@@ -4,13 +4,14 @@ c200 <- load_DVF("c200") %>% st_drop_geometry()
 
 ttrr5_emp09_200 <- load_DVF("iso200/ttrr5_emp09_200")
 idf_dt <- r2dt(ttrr5_emp09_200)
-idf_dt <- merge(idf_dt, c200[, c("idINS200", "Ind")] %>% st_drop_geometry(), by.x="idINS", by.y="idINS200")
+idf_dt <- merge(idf_dt, c200[, c("idINS200", "Ind")] , by.x="idINS", by.y="idINS200")
 Nidf <- idf_dt[, sum(Ind)] # 10M normal !
 idf <- ggplot(idf_dt, aes(x=to25k, y=..density..*Nidf, weight=Ind))+geom_density()
 
 ttrr5_emp09_lyon <- load_DVF("ttr_r5_emp09_Lyon")
 lyon_dt <- r2dt(ttrr5_emp09_lyon, 200)
 lyon_dt <- merge(lyon_dt, c200[, c("idINS200", "Ind")], by.x="idINS200", by.y="idINS200")  # ajoute la colonne Ind aux données
+setnames(lyon_dt, "idINS200", "idINS")
 Nlyon <- lyon_dt[, sum(Ind)] # total des habitants, 21858 étrange ?
 # méthode 1, 2 graphs
 idf <- ggplot(idf_dt, aes(x=to25k, y=..density..*Nidf, weight=Ind))+geom_density()
