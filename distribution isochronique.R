c170c1e69d9aeccd9562f0ec05c240f7d80a4e5d
xtimbeau <54633745+xtimbeau@users.noreply.github.com>
2020-11-20 16:29:07 +0100

    Update distribution isochronique.R

--- distribution isochronique.R
+++ distribution isochronique.R
@@ -46,8 +46,13 @@ idf_dt <- r2dt(ttrr5_emp09_200) %>% merge(c200[, .(idINS200, Ind)], by="idINS200
 ggplot(disichrone(idf_dt, to500k, Ind))+geom_area(aes(x=to500k, y=Ind))
 
 # méthode 2, en utilisant geom_massity comme geom_density, mais avec un calcul en plus: mass et cummass
-# on définit les distances à grapher (dans l'ordre que l'on souhaite) 
+# on définit les données et les distances à grapher (dans l'ordre que l'on souhaite) 
+idf_dt <- r2dt(load_DVF("iso200/ttrr5_emp09_200"))
+idf_dt <- merge(idf_dt, c200[, .(idINS200, Ind)], by="idINS200")
+lyon_dt <- r2dt(load_DVF("ttr_r5_emp09_Lyon"), 200) # le 200 indique qu'on recalcule le raster en résolution 200
+lyon_dt <- merge(lyon_dt, c200[, .(idINS200, Ind)], by="idINS200")
 distances <-c("to25k", "to50k","to100k","to150k","to200k","to250k")
+
 # melt transforme idf_dt en format long
 idf_dtm <- idf_dt[, .SD, .SDcols=c(distances, "Ind")] %>% melt(measure.vars=distances, variable.name="seuil", value.name="dist")
 lyon_dtm <- lyon_dt[, .SD, .SDcols=c(distances, "Ind")] %>% melt(measure.vars=distances, variable.name="seuil", value.name="dist")
@@ -55,6 +60,12 @@ lyon_dtm <- lyon_dt[, .SD, .SDcols=c(distances, "Ind")] %>% melt(measure.vars=di
 ggplot(idf_dtm)+geom_massity(aes(x=dist, mass=Ind, y=after_stat(mass), col=seuil, fill=seuil), alpha=0.5)
 # mais aussi comme facette
 ggplot(idf_dtm)+geom_massity(aes(x=dist, mass=Ind, y=after_stat(mass), col=seuil, fill=seuil))+facet_wrap(~seuil)
+# mais aussi comme facette et cumulé
+ggplot(idf_dtm)+geom_massity(aes(x=dist, mass=Ind, y=after_stat(cummass), col=seuil, fill=seuil))+facet_wrap(~seuil)
 # ou en mélangeant lyon et paris
 data <- rbind(idf_dtm[, ville:="paris"], lyon_dtm[, ville:="lyon"])
 ggplot(data)+geom_massity(aes(x=dist, mass=Ind, y=after_stat(mass), col=ville, fill=ville), alpha=0.5)+facet_wrap(~seuil)
+# on peut vouloir des fonctions en %
+ggplot(data)+geom_massity(aes(x=dist, mass=Ind, y=after_stat(mass)/after_stat(total_mass), col=ville, fill=ville), alpha=0.5)+facet_wrap(~seuil)
+# on peut vouloir des fonctions en % et en cumul
+ggplot(data)+geom_massity(aes(x=dist, mass=Ind, y=after_stat(cummass)/after_stat(total_mass), col=ville, fill=ville), alpha=0.5)+facet_wrap(~seuil)
