62590b682b45b14a70004ff63d1c0715df9cf78e
Arij-Hamouda <80691535+Arij-Hamouda@users.noreply.github.com>
2021-03-23 16:26:50 -0100

    Add files via upload

---
+++ Arij/LaRochelle_OSM_découpé.r
@@ -0,0 +1,18 @@
+source ("init.r")
+
+iris15 <- load_DVF("iris15") %>% st_transform(3035)
+als <- iris15 %>% filter(UU2010=="17601") %>% st_buffer(20000) %>% st_union 
+iris_als <- iris15 %>% filter(st_within(iris15,als, sparse=FALSE)) 
+als_box <- st_bbox(als %>% st_transform(4326)) 
+
+infile <- "france-latest.osm.pbf" %>% glue() 
+outfile <- "larochelle.osm.pbf" %>% glue 
+
+unfiltered <- str_c("osmosis.bat --read-pbf {infile}",
+                    "--bounding-box left={als_box$xmin} bottom={als_box$ymin} right={als_box$xmax} top={als_box$ymax}","--write-pbf {outfile}", sep=" ") %>% glue
+
+
+current.wd <- getwd()
+setwd("G:/Mon Drive/DVFdata/sources/fdCartes/OSM" %>% glue)
+system2("powershell.exe", args = unfiltered)
+setwd(current.wd)
