8b60a919469c43ab77659d3508faf77ce942ec93
xtimbeau <54633745+xtimbeau@users.noreply.github.com>
2021-03-23 08:50:48 +0100

    Pour Arij

---
+++ Arij/R5-make isocar&isotransit Marseille.R
@@ -0,0 +1,74 @@
+source("init.r") # initialitation
+
+# source des données d'opportunité: emploi au niveau IRIS, en 2009, source CLAP+INSEE
+# La prochaine étape sera de construire à partir de FLORES les opportunités à un niveau plus fin
+iris15 <- load_DVF("iris15")
+
+# sélection géographique des données d'opportunité à l'aire urbaine+20km histoire de ne manquer personne
+paca <- iris15 %>% filter(UU2010=="00759") %>% st_buffer(20000) %>% st_union
+uu759 <- iris15 %>% filter(UU2010=="00759") %>% st_union
+iris15_paca <- iris15 %>% select(EMP09, P15_POP) %>% filter(st_within(.,paca, sparse=FALSE)) %>% st_centroid()
+
+# carreaux sélectionnés pour le calcul de la grille
+# On ne calcule pas les isochrones pour des carreaux inhabités
+# Pour l'INSEE le nombre de ménages par carreau est supérieur à 10
+
+c200 <- load_DVF("c200") 
+c200_759 <- c200 %>% filter(st_within(., uu759, sparse=FALSE))
+
+# Moteur r5, en transit
+# Attention à la date qui doit être une date existante dans le GTFS/calendar.txt, un jour de semaine, si possible sans congés
+
+tr_r5_Marseille <- routing_setup_r5(path="{DVFdata}/r5r_data/Marseille/r5" %>% glue, mode=c("WALK", "TRANSIT"),
+                                    time_window=60,montecarlo = 100,percentiles = 5L,n_threads=4, date="15-10-2020 8:00:00")
+
+iso_transit_50_r5_Marseille <- iso_accessibilite(
+    quoi=iris15_paca, # les variables d'opportunité
+    ou=c200_759, # la grille cible 
+    resolution=50, # la résolution finale (le carreau initial est de 200m, il est coupé en 16 pour des carreaux de 50m)
+    tmax=120, # le temps max des isochrones en minutes
+    pdt=1, # le pas de temps pour retourner le résultat en minute
+    routing=tr_r5_Marseille) # moteur de routing initialisé plus haut
+
+save_DVF(iso_transit_50_r5_Marseille)
+
+#Visualisation du résultat
+tm_shape(iso_transit_50_r5_Marseille$EMP09$iso45m)+tm_raster(style="cont", palette=heatrg)
+
+
+# Pour mettre en place r5 ---------------
+
+# il faut installer le sdk java v11 (https://www.oracle.com/java/technologies/javase-jdk11-downloads.html) ,
+# puis le package R rJava
+# la version de développement de r5
+
+# devtools::install_github("ipeaGIT/r5r", subdir = "r-package")
+
+# il faut ensuite créer un dossier où se trouvent le pbf (OSM découpé) et le ou les GTFS pour le transport
+# c'est ce dossier que lon passe ensuite à routing_setup_r5
+
+
+
+# Pour mettre en place OSRM --------------
+
+# il faut installer Docker et WSL2
+# il faut ensuite créer un dossier avec le pdf d'OSM découpé
+# Puis il faut en se plaçant dans le dossier et en ouvrant powershell lancer les commandes suivantes:
+# 
+# Normalement charge de docker hub les fichiers nécessaires
+# on défini ici le profil de déplacement (/opt/car.lua pour la voiture, /opt/foot.lua pour la marche, etc...)
+# si on modifie le profile car.lua, il faut le placer dans le dossier et changer /opt/car.lua en /data/car.lua
+# c'est ce qui est fait ici le car.lua a été modifé pour augmenter la pénalité de feu rouge de 2s à 20s
+# dans cet exemple le pdf s'appelle Idf-latest.osm.pdf
+
+# docker run -t -v "${PWD}:/data" osrm/osrm-backend osrm-extract -p /data/car.lua /data/Idf-latest.osm.pbf
+# docker run -t -v "${PWD}:/data" osrm/osrm-backend osrm-partition /data/Idf-latest.osrm
+# docker run -t -v "${PWD}:/data" osrm/osrm-backend osrm-customize /data/Idf-latest.osrm
+
+# fabrique le coneneur, que lon' peut relancer à chaque fois (pas besoin de le reconstruire)
+# on lui donne un nom, pour le reconnaitre et un numéro de port (qui transfère 5002 vers 5000 qui est utilisé dans le conteneur) pour avoir plusieurs
+# conteneurs qui correspondent à plusieurs options (voiture, voiture avec pénalité feu rouge, à pied, en vélo)
+# il faut vérifier que le conteneur est lancé dans docker et le relancer sinon
+
+# docker run -t -i --name=osrm_idf_car_t -p 5002:5000 -v "${PWD}:/data" osrm/osrm-backend osrm-routed --algorithm mld --max-table-size 1000000 --threads 8 /data/Idf-latest.osrm
+
